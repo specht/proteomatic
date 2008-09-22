a8273c40380bfac889ddd56a21530d546f2c1f74
Michael Specht <micha.specht@gmail.com>
2008-09-22 20:21:52 +0000

    SimQuant script cont'd

--- scripts/simquant.rb
+++ scripts/simquant.rb
@@ -22,6 +22,7 @@ class SimQuant < ProteomaticScript
 		end
 		
 		lk_Peptides.uniq!
+		lk_Peptides.collect! { |x| x.upcase }
 		if lk_Peptides.empty?
 			puts 'Error: no peptides have been specified.'
 			exit 1
@@ -33,7 +34,7 @@ class SimQuant < ProteomaticScript
 		FileUtils::mkpath(ls_TempPath)
 		FileUtils::mkpath(ls_SvgPath)
 		
-		ls_Command = "#{ExternalTools::binaryPath('simquant.simquant')} --textOutput no --yamlOutput yes --yamlOutputTarget #{ls_YamlPath} --svgOutPath #{ls_SvgPath} --files #{@input[:spectra].join(' ')} --peptides #{lk_Peptides.join(' ')}"
+		ls_Command = "#{ExternalTools::binaryPath('simquant.simquant')} --cropUpper #{@param[:cropUpper] / 100.0} --textOutput no --yamlOutput yes --yamlOutputTarget #{ls_YamlPath} --svgOutPath #{ls_SvgPath} --files #{@input[:spectra].join(' ')} --peptides #{lk_Peptides.join(' ')}"
 		puts 'There was an error while executing simquant.' unless system(ls_Command)
 		
 		lk_Results = YAML::load_file(ls_YamlPath)
@@ -44,7 +45,7 @@ class SimQuant < ProteomaticScript
 				lk_Out.puts "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.1//EN' 'http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd'>"
 				lk_Out.puts "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='de'>"
 				lk_Out.puts '<head>'
-				lk_Out.puts '<title>SIM Quantitation Results</title>'
+				lk_Out.puts '<title>SimQuant Report</title>'
 				lk_Out.puts '<style type=\'text/css\'>'
 				lk_Out.puts 'body {font-family: Verdana; font-size: 10pt;}'
 				lk_Out.puts 'h1 {font-size: 14pt;}'
@@ -65,10 +66,33 @@ class SimQuant < ProteomaticScript
 				lk_Out.puts '.toggle { text-decoration: underline; color: #aaa; }'
 				lk_Out.puts '.toggle:hover { cursor: pointer; color: #000; }'
 				lk_Out.puts '</style>'
+				lk_Out.puts "<script type='text/javascript'>"
+				lk_Out.puts "<![CDATA["
+				lk_Out.puts "function toggle(as_Name) {"
+				lk_Out.puts "lk_Elements = document.getElementsByClassName(as_Name);"
+				lk_Out.puts "for (var i = 0; i < lk_Elements.length; ++i)"
+				lk_Out.puts "lk_Elements[i].style.display = lk_Elements[i].style.display == 'none' ? 'table-row' : 'none';"
+				lk_Out.puts "}"
+				lk_Out.puts "]]>"
+				lk_Out.puts "</script>"
 				lk_Out.puts '</head>'
 				lk_Out.puts '<body>'
+				lk_Out.puts "<h1>SimQuant Report</h1>"
+				lk_Out.puts '<p>'
+				lk_FoundPeptides = lk_Results['results'].keys
+				lk_NotFoundPeptides = lk_Peptides.reject { |x| lk_FoundPeptides.include?(x) }
+				lk_Out.puts "Searched for #{lk_Peptides.size} peptide#{lk_Peptides.size != 1 ? 's' : ''} in #{@input[:spectra].size} file#{@input[:spectra].size != 1 ? 's' : ''}, trying charge states #{@param[:minCharge]} to #{@param[:maxCharge]} and merging the upper #{@param[:cropUpper]}% of all scans in which a peptide was found.<br />"
+				lk_Out.puts "#{lk_FoundPeptides.size} of these peptides were quantified, #{lk_NotFoundPeptides.size} have not been found."
+				lk_Out.puts '</p>'
+				lk_Out.puts '<h2>Contents</h2>'
+				lk_Out.puts '<ol>'
+				lk_Out.puts "<li><a href='#header-quantified-peptides'>Quantified peptides</a></li>"
+				lk_Out.puts "<li><a href='#header-unidentified-peptides'>Unidentified peptides</a></li>"
+				lk_Out.puts '</ol>'
+				lk_Out.puts "<h2 id='header-quantified-peptides'>Quantified peptides</h2>"
 				lk_Out.puts "<table style='min-width: 820px;'>"
 				lk_Peptides = lk_Results['results'].keys.sort
+				li_StaticToggleCounter = 0
 				lk_Peptides.each do |ls_Peptide|
 					lk_Out.puts "<tr><td colspan='5' style='border: none; padding-top: 1em; padding-bottom: 1em;'><b>#{ls_Peptide}</b></td></tr>"
 					lk_Out.puts "<tr><th>Spot</th><th>Charge</th><th>Ratio</th><th>Certainty</th><th>Scans</th></tr>"
@@ -80,27 +104,34 @@ class SimQuant < ProteomaticScript
 					end
 					lk_PeptideResults.each do |lk_Row|
 						lk_SubRows = lk_Row['scans']
-						lk_SubRows.sort! { |a, b| b['score'] <=> a['score'] }
-						lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_Row['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_Row['certainty'].to_f * 100.0)} %</td><td><span class='toggle'>#{lk_SubRows.size} scans</span></td></tr>"
+						lk_SubRows.sort! { |a, b| a['retentionTime'] <=> b['retentionTime'] }
+						li_StaticToggleCounter += 1
+						ls_Name = "scan-#{li_StaticToggleCounter}"
+						lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_Row['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_Row['certainty'].to_f * 100.0)} %</td><td><span class='toggle' onclick=\"toggle('#{ls_Name}')\">#{lk_SubRows.size} scans</span></td></tr>"
 						lk_SubRows.each do |lk_SubRow|
 							ls_Svg = File::read(File::join(ls_SvgPath, lk_SubRow['svg'] + '.svg'))
 							ls_Svg.sub!(/<\?xml.+\?>/, '')
-							ls_Svg.sub!(/<svg width=\".+\" height=\".+\"/, '<svg ')
-							lk_Out.puts "<tr class='sub'><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_SubRow['certainty'].to_f * 100.0)} %</td><td></td></tr>"
-							lk_Out.puts "<tr class='sub'><td colspan='5'>"
+							ls_Svg.sub!(/<svg width=\".+\" height=\".+\"/, "<svg ")
+							lk_Out.puts "<tr class='sub #{ls_Name}' style='display: none;'><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_SubRow['certainty'].to_f * 100.0)} %</td><td></td></tr>"
+							lk_Out.puts "<tr class='sub #{ls_Name}' style='display: none;'><td colspan='5'>"
+							lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{sprintf("%1.2f", lk_SubRow['retentionTime'].to_f)} minutes</div>"
 							lk_Out.puts ls_Svg
-							lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{lk_SubRow['retentionTime'].to_f} minutes</div>"
 							lk_Out.puts "</td></tr>"
 						end
 					end
 				end
 				lk_Out.puts '</table>'
+				lk_Out.puts "<h2 id='header-unidentified-peptides'>Unidentified peptides</h2>"
+				lk_Out.puts "<table><tr><th>Peptide</th></tr>"
+				lk_NotFoundPeptides.sort!
+				lk_NotFoundPeptides.each do |ls_Peptide|
+					lk_Out.puts "<tr><td>#{ls_Peptide}</td></tr>"
+				end
+				lk_Out.puts "</table>"
 				lk_Out.puts '</body>'
 				lk_Out.puts '</html>'
 			end
 		end
-		puts "AAAAAAARGH! CLANK CLANK YOU'RE DEAD."
-		exit 1
 	end
 end
 
