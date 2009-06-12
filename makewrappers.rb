d6f6e2c815fb69a0493fb8bbc3d6dd34afb302ca
Michael Specht <micha.specht@gmail.com>
2009-06-12 18:31:31 +0000

    updated makewrappers for Windows

--- makewrappers.rb
+++ makewrappers.rb
@@ -3,6 +3,7 @@ ls_OldDir = Dir.pwd
 print 'Building wrappers...'
 Dir.chdir('src/src')
 ['Proteomatic', 'ProteomaticPipeline', 'Revelio'].each do |ls_Tool|
+	ls_Tool += '.exe' if RUBY_PLATFORM.downcase.include?('mswin')
 	system("gcc -o ../../#{ls_Tool} -DBINARY=\\\"#{ls_Tool}\\\" BinaryWrapper.cpp -lstdc++")
 end
 puts 'done.'
