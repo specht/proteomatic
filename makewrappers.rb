9516356e0c4358c34fa9ee56d70c98851325c4e5
Michael Specht <micha.specht@gmail.com>
2009-07-13 09:21:01 +0000

    added Windows icons to wrapper programs

--- makewrappers.rb
+++ makewrappers.rb
@@ -2,7 +2,7 @@ lb_Windows = RUBY_PLATFORM.downcase.include?('mswin')
 print 'Building wrappers...'
 ['Proteomatic', 'ProteomaticPipeline', 'Revelio'].each do |ls_Tool|
 	ls_Tool += '.exe' if lb_Windows
-	system("gcc -o #{ls_Tool} -DBINARY=\\\"#{ls_Tool}\\\" -D#{lb_Windows ? "WIN32" : "WHATEVER"} src/src/BinaryWrapper.cpp -lstdc++")
+	system("gcc -o #{ls_Tool} -DBINARY=\\\"#{ls_Tool}\\\" -D#{lb_Windows ? "WIN32" : "WHATEVER"} src/src/BinaryWrapper.cpp src/obj/release/#{ls_Tool.sub('.exe', '')}_res.o -lstdc++")
 	system("strip --strip-all #{ls_Tool}")
 end
 puts 'done.'
